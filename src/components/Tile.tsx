f9d84e878f37664d6738c32b566daf30477f89aa
Senne van Doorn <senne@squaredpaper.co.uk>
2025-04-12 13:32:15 +0100

    New tile_map.html did not load

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -8,7 +8,7 @@ const Tiles: React.FC = () => {
   const mapContent = (
     <div style={{ width: "100vw", height: "70vh", maxWidth: "100%" }}>
       <iframe 
-        src="/flowchart/tile_map.html"
+        src="/flowchart/small_tile_map.html"
         style={{ width: "100%", height: "100%", border: "none" }} 
         title="Map"
       />
