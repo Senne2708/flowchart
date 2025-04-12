9042ad10645f9b09caaac7701fadaacc8390c3da
Senne van Doorn <senne@squaredpaper.co.uk>
2025-04-12 09:07:44 +0100

    Removing control from tile image

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -8,7 +8,7 @@ const Tiles: React.FC = () => {
   const mapContent = (
     <div style={{ width: "100vw", height: "80vh", maxWidth: "100%" }}>
       <iframe 
-        src="/flowchart/temp_map.html"
+        src="/flowchart/tile_map.html"
         style={{ width: "100%", height: "100%", border: "none" }} 
         title="Map"
       />
