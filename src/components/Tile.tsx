b036c162e7b5c4614dac4fa14706f95ef5440808
Senne van Doorn <senne@squaredpaper.co.uk>
2025-04-12 08:10:48 +0100

    fix bug

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -9,7 +9,7 @@ const Tiles: React.FC = () => {
   const mapContent = (
     <div style={{ width: "100vw", height: "80vh", maxWidth: "100%" }}>
       <iframe 
-        src="/flowchart/public/temp_map.html" 
+        src="https://senne2708.github.io/flowchart/public/temp_map.html" 
         style={{ width: "100%", height: "100%", border: "none" }} 
         title="Map"
       />
