5db88af08728e24fde6b29b02ecf8859ee06ee90
Senne van Doorn <senne@squaredpaper.co.uk>
2025-04-14 11:35:28 +0100

    changing format

--- src/components/QualityFiltering.tsx
+++ src/components/QualityFiltering.tsx
@@ -1,4 +1,4 @@
-import { Button, Text, Grid, Image, Title } from "@mantine/core";
+import { Button, Text, Grid, Image, Title, Stack } from "@mantine/core";
 import { useModal } from "../context/modalContext";
 import { colors } from "./colors";
 import React from "react";
@@ -15,181 +15,127 @@ const QualityFiltering: React.FC<{type: string, content?: string}> = ({type, con
           <Title order={3} mb="md">Dead Pixels</Title>
             <Text>No dead pixels in this collection</Text>
           <Title order={3} mb="md">Cloud Segmentation</Title>
-          <Grid gutter="md" maw={800}>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_1.jpeg" 
-                alt="Result 1" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_2.jpeg" 
-                alt="Result 2" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_3.jpeg" 
-                alt="Result 3" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_4.jpeg" 
-                alt="Result 4" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_5.jpeg" 
-                alt="Result 5" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_6.jpeg" 
-                alt="Result 6" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_7.jpeg"
-                alt="Result 7" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_8.jpeg" 
-                alt="Result 8" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="mask_cloud_pred/images_9.jpeg" 
-                alt="Result 9" 
-              />
-            </Grid.Col>
-          </Grid>
+          <Stack>
+            <Image 
+              src="mask_cloud_pred/images_1.jpeg" 
+              alt="Result 1" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_2.jpeg" 
+              alt="Result 2" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_3.jpeg" 
+              alt="Result 3" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_4.jpeg" 
+              alt="Result 4" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_5.jpeg" 
+              alt="Result 5" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_6.jpeg" 
+              alt="Result 6" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_7.jpeg"
+              alt="Result 7" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_8.jpeg" 
+              alt="Result 8" 
+            />
+            <Image 
+              src="mask_cloud_pred/images_9.jpeg" 
+              alt="Result 9" 
+            />
+          </Stack>
         </>
       );
     } else if (type === 'dataset') {
       modalContent = (
       <>
         <Title order={3} mb="md">Dead Pixels</Title>
-          <Grid gutter="md" maw={800}>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_1.jpeg" 
-                alt="Result 1" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_2.jpeg" 
-                alt="Result 2" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_3.jpeg" 
-                alt="Result 3" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_4.jpeg" 
-                alt="Result 4" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_5.jpeg" 
-                alt="Result 5" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_6.jpeg" 
-                alt="Result 6" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_7.jpeg"
-                alt="Result 7" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_8.jpeg" 
-                alt="Result 8" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_deadpixels/images_9.jpeg" 
-                alt="Result 9" 
-              />
-            </Grid.Col>
-          </Grid>
+          <Stack>
+            <Image 
+              src="dataset_deadpixels/images_1.jpeg" 
+              alt="Result 1" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_2.jpeg" 
+              alt="Result 2" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_3.jpeg" 
+              alt="Result 3" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_4.jpeg" 
+              alt="Result 4" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_5.jpeg" 
+              alt="Result 5" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_6.jpeg" 
+              alt="Result 6" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_7.jpeg"
+              alt="Result 7" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_8.jpeg" 
+              alt="Result 8" 
+            />
+            <Image 
+              src="dataset_deadpixels/images_9.jpeg" 
+              alt="Result 9" 
+            />
+          </Stack>
         <Title order={3} mb="md">Cloud Segmentation</Title>
-          <Grid gutter="md" maw={800}>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_1.jpeg" 
-                alt="Result 1" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_2.jpeg" 
-                alt="Result 2" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_3.jpeg" 
-                alt="Result 3" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_4.jpeg" 
-                alt="Result 4" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_5.jpeg" 
-                alt="Result 5" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_6.jpeg" 
-                alt="Result 6" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_7.jpeg"
-                alt="Result 7" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_8.jpeg" 
-                alt="Result 8" 
-              />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Image 
-                src="dataset_cloud_pred/images_9.jpeg" 
-                alt="Result 9" 
-              />
-            </Grid.Col>
-          </Grid>
+          <Stack>
+            <Image 
+              src="dataset_cloud_pred/images_1.jpeg" 
+              alt="Result 1" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_2.jpeg" 
+              alt="Result 2" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_3.jpeg" 
+              alt="Result 3" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_4.jpeg" 
+              alt="Result 4" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_5.jpeg" 
+              alt="Result 5" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_6.jpeg" 
+              alt="Result 6" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_7.jpeg"
+              alt="Result 7" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_8.jpeg" 
+              alt="Result 8" 
+            />
+            <Image 
+              src="dataset_cloud_pred/images_9.jpeg" 
+              alt="Result 9" 
+            />
+          </Stack>
       </>
       );
     } else {
